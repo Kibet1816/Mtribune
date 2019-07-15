8f8826a795a5b70d9af13245b740e39a5fb0a89a
Denis Kibet <kibet1816@gmail.com>
2019-07-15 09:54:15 +0300

    Add welcome view function

--- news/views.py
+++ news/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def welcome(request):
+    """
+    View function to render the welcome page
+
+    Args:
+        Request:Contains info of the current web request that has triggered hte view
+    """
+    return HttpResponse('Welcome to the Moringa Tribune')
