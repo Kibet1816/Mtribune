9c50bbb746c89056e067c9a4a048b1fb17711b42
Denis Kibet <kibet1816@gmail.com>
2019-07-16 15:19:57 +0300

    Test article class

--- news/models.py
+++ news/models.py
@@ -1,30 +1,57 @@
+import datetime as dt
 from django.db import models
 
 # Create your models here.
+
+
 class Editor(models.Model):
     """
     Editor model class
     """
-    first_name = models.CharField(max_length = 30)
-    last_name = models.CharField(max_length = 30)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
     email = models.EmailField()
+    phone_number = models.CharField(max_length=10, blank=True)
 
     def __str__(self):
         return self.first_name
 
+    def save_editor(self):
+        self.save()
+
+    def delete_editor(self):
+        self.delete()
+
     class Meta:
         ordering = ['first_name']
+
 
 class tags(models.Model):
     """
     Tags model class
     """
+    name = models.CharField(max_length=200, default='testing')
+
     def __str__(self):
         return self.name
 
+
 class Article(models.Model):
-    title = models.CharField(max_length = 60)
+    title = models.CharField(max_length=60)
     post = models.TextField()
     editor = models.ForeignKey(Editor)
-    tags = models.ManyToManyField(tags)
-    pub_date = models.DateTimeField(auto_now_add = True)
+    tags = models.ManyToManyField(tags, blank=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    @classmethod
+    def todays_news(cls):
+        today = dt.date.today()
+        news = cls.objects.filter(pub_date__date=today)
+
+        return news
+
+    @classmethod
+    def date_news(cls, date):
+        news = cls.objects.filter(pub_date__date=date)
+
+        return news
--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,72 @@
+import datetime as dt
 from django.test import TestCase
+from .models import Editor, Article, tags
 
 # Create your tests here.
+
+
+class EditorTestClass(TestCase):
+    """
+    Test class for the editor model class
+    """
+
+    def setUp(self):
+        """
+        Creates instance of Editor module before each test
+        """
+        self.denis = Editor(first_name='Denis',
+                            last_name='Kibet', email='kibet@gmail.com')
+
+    def test_instance(self):
+        """
+        Test method to check for correct instantiation
+        """
+        self.assertTrue(isinstance(self.denis, Editor))
+
+    def test_save_method(self):
+        self.denis.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)
+
+    def test_delete_method(self):
+        self.denis.save_editor()
+        self.denis.delete_editor()
+        noeditors = Editor.objects.all()
+        self.assertTrue(len(noeditors) == 0)
+
+
+class ArticleTestClass(TestCase):
+    """
+    Test class for the Article module
+    """
+
+    def setUp(self):
+        """
+        """
+        self.rule = Editor(first_name='Ja', last_name='Rule',
+                           email='ja@rule.com')
+        self.rule.save_editor()
+
+        self.new_tag = tags(name='testing')
+        self.new_tag.save()
+
+        self.new_article = Article(
+            title='Test Article', post='This is a random test Post', editor=self.rule)
+        self.new_article.save()
+
+        self.new_article.tags.add(self.new_tag)
+
+    def tearDown(self):
+        Editor.objects.all().delete()
+        tags.objects.all().delete()
+        Article.objects.all().delete()
+
+    def test_get_news_today(self):
+        today_news = Article.todays_news()
+        self.assertTrue(len(today_news) > 0)
+
+    def test_get_news_by_date(self):
+        test_date = '2019-07-16'
+        date = dt.datetime.strptime(test_date, '%Y-%m-%d').date()
+        news_by_date = Article.date_news(date)
+        self.assertTrue(len(news_by_date) > 0)
