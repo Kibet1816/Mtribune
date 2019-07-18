b5884bb0fb3fbfba569a8a219578cd6ae3737397
Denis Kibet <kibet1816@gmail.com>
2019-07-18 15:06:51 +0300

    Deploy

--- tribune/settings.py
+++ tribune/settings.py
@@ -20,22 +20,22 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 from decouple import config, Csv
 MODE = config("MODE", default='dev')
 SECRET_KEY = config('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
-
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # SECRET_KEY = 'gorgonsonofskrygon'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
-
+# DEBUG = True
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'gorgonsonofskrygon'
-# SECURITY WARNING: don't run with debug turned on in production!
 
-# ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -92,8 +92,8 @@ if config('MODE') == 'dev':
             'NAME': config('DB_NAME'),
             'USER': config('DB_USER'),
             'PASSWORD': config('DB_PASSWORD'),
-            'HOST': config('DB_HOST'),
-            'PORT': '',
+            'HOST':config('DB_HOST'),
+            'PORT':'',
         }
     }
 else:
